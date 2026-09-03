38ba686616b289884e9cd9663a81c118ed3a88ae
AquiniG <aquini94@gmail.com>
2026-09-03 13:21:18 -0300

    Adiciona testando.kt e testando.txt

---
+++ testando.kt
@@ -0,0 +1,36 @@
+fun main() {
+
+
+    val titulo = "As Tartarugas Ninjas"
+
+
+    val nome1 = "Leonardo"
+    val arma1 = "Katanas"
+    val cor1 = "Azul"
+    var peso1 = "94.2kg"
+    val altura1 = "168cm"
+
+
+    val nome2 = "Raphael"
+    val arma2 = "Adagas"
+    val cor2 = "Vermelho"
+    var peso2 = "90.8kg"
+    val altura2 = "166cm"
+
+
+    println(titulo)
+
+    println("Nome: $nome1")
+    println("Arma: $arma1")
+    println("Cor: $cor1")
+    println("Peso: $peso1")
+    println("Altura: $altura1")
+
+    println("Nome: $nome2")
+    println("Arma: $arma2")
+    println("Cor: $cor2")
+    println("Peso: $peso2")
+    println("Altura: $altura2")
+
+
+}
